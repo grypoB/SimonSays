6c10df2c16488e97d715c34d45861df3febdeb43
Alexandre Devienne <alexandre@TMA-1.wv.cc.cmu.edu>
2017-04-15 13:31:49 -0400

    Debuged pi button input

--- testButtons.py
+++ testButtons.py
@@ -1,17 +1,22 @@
-mport RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
-
-pin = [17,18,27,22]
+n = 4
+pin = [17,18,23,24]
 old_state = [0, 0, 0, 0]
 cur_state = [0, 0, 0, 0]
 
 GPIO.setmode(GPIO.BCM)
 
-for i in xrange(0,4):
+for i in xrange(0,n):
     GPIO.setup(pin[i], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-    for i in xrange(0,4):
+    #print old_state
+    #print cur_state 
+    for i in xrange(0,n):
         cur_state[i] = GPIO.input(pin[i])
-        if cur_state[i]==False and old_state[i]==True:
+        #print i, " ", cur_state[i] 
+        if cur_state[i]==0 and old_state[i]==1:
             print i, " on rising edge"
+    old_state = cur_state[:]
+    #time.sleep(1)
