d216e2421c25c8c2744f7d3ac7e61009da9b854e
Alexandre Devienne <alexandre@TMA-1.wv.cc.cmu.edu>
2017-05-10 12:05:46 -0400

    Modified button_test.py for in booth testting

--- testButtons.py
+++ testButtons.py
@@ -1,9 +1,13 @@
 import RPi.GPIO as GPIO
 import time
-n = 4
-pin = [17,18,23,24]
-old_state = [0, 0, 0, 0]
-cur_state = [0, 0, 0, 0]
+#n = 4
+#pin = [17,18,23,24]
+n = 1
+pin = [22]
+#old_state = [0, 0, 0, 0]
+#cur_state = [0, 0, 0, 0]
+cur_state = [0]
+old_state = [0]
 
 GPIO.setmode(GPIO.BCM)
 
@@ -12,7 +16,7 @@ for i in xrange(0,n):
 
 while True:
     #print old_state
-    #print cur_state 
+    print cur_state 
     for i in xrange(0,n):
         cur_state[i] = GPIO.input(pin[i])
         #print i, " ", cur_state[i] 
