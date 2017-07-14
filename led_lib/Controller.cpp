c17fe7268d87298d2788eec22212911ef1b43d76
Alexandre Devienne <alexandre@TMA-1.local>
2017-07-13 22:14:33 -0400

    Restructure Controller class into .cpp and .h
    Ordered the functions in a better order, no real doc yet, but it should
    be more readable now.
    Removed some trailing semicolons

--- led_lib/Controller.cpp
+++ led_lib/Controller.cpp
@@ -1,113 +1,200 @@
 #include "Controller.h"
 
+Controller::Controller(Color nStrip[], int16_t nSize, Color nMainColor)
+    : mainColor(nMainColor), size(nSize), colort(nStrip) {};
 
 void Controller::update(uint32_t now) {
     nowTic = now;
-    
+
     switch (mode) {
         case MODE_AUTO_HSV:
         case MODE_AUTO: updateAuto();
-        break;
+            break;
         case MODE_BLINK: updateBlink();
-        break;
+            break;
         case MODE_BREATH: updateBreath();
-        break;
+            break;
         case MODE_AUTO_HSV_SINGLE: updateAutoSingle();
-        break;
+            break;
         case MODE_FILL_HSV:
         case MODE_FILL_HSV_REVERSE: updateFillHSV();
-        break;
+            break;
         case MODE_EMPTY:
         case MODE_EMPTY_REVERSE: updateEmpty();
-        break;
+            break;
         case MODE_MANUAL: setColor(color1);
-        break;
+            break;
     }
 
-    
-    switch (effect) {
-      case EFFECT_BEAM:
-      case EFFECT_BEAM_REVERSE: updateBeam();
-      break;
-      case EFFECT_FLASH: updateFlash();
-      case EFFECT_NONE: //don't do anything
-      break;
-    
+
+    if (effect != EFFECT_NONE) {
+        if (effectTic == 0) {
+            // happens in case of lag
+            // some effects timer are set to 0 to insure they play in full
+            effectTic = nowTic;
+        }
+
+        switch (effect) {
+            case EFFECT_BEAM:
+            case EFFECT_BEAM_REVERSE: updateBeam();
+                break;
+            case EFFECT_FLASH: updateFlash();
+            case EFFECT_NONE: //don't do anything
+                break;
+        }
     }
-      
-    //setColorSingle(colort);
 }
 
 
+// ============================================================================
+// Set mode
+// ============================================================================
 
-void Controller::updateBreath() {
-    Color color = color1;
-    uint32_t spent = nowTic - lastTic;
-    if (spent < delay1) {
-        color.fade(color1, color2, spent, 0, delay1);
-    } else if (spent < delay1+delay2) {
-        color.fade(color2, color1, spent, delay1, delay1+delay2);
+// ------------------------- Full mode ----------------------------------------
+
+void Controller::setBreath(Color nColor1, Color nColor2,
+                           uint16_t upTime, uint16_t downTime) {
+    mode = MODE_BREATH;
+    color1 = nColor1;
+    color2 = nColor2;
+
+    delay1 = upTime;
+    delay2 = downTime;
+}
+
+
+void Controller::setAuto(uint16_t nTransition, uint16_t nStable, uint8_t nBrightness) {
+    mode = MODE_AUTO;
+
+    brightness = nBrightness;
+    transition = nTransition;
+    stable     = nStable;
+}
+
+
+void Controller::setAutoHSV(uint16_t nTransition, uint16_t nStable) {
+    mode = MODE_AUTO_HSV;
+
+    transition = nTransition;
+    stable     = nStable;
+}
+
+
+void Controller::setManual(Color color) {
+    mode = MODE_MANUAL;
+    color1 = color;
+}
+
+
+void Controller::setBlink(Color nColor1, Color nColor2, uint32_t nDelay1, uint32_t nDelay2) {
+    mode = MODE_BLINK;
+
+    color1 = nColor1;
+    color2 = nColor2;
+
+    delay1 = nDelay1;
+    delay2 = nDelay2;
+}
+
+
+// ------------------------- Set effect ---------------------------------------
+
+void Controller::setEffectFlash(Color color, uint32_t duration) {
+    effect = EFFECT_FLASH;
+
+    effectColor = color;
+    effectDuration = duration;
+
+    effectTic = nowTic;
+}
+
+
+void Controller::setEffectBeam(Color color, uint16_t radius, uint16_t speed, bool reverse) {
+    if (reverse) {
+        effect = EFFECT_BEAM_REVERSE;
     } else {
-        lastTic = nowTic;
+        effect = EFFECT_BEAM;
     }
-    
-    setColor(color);
+    effectColor = color;
+    beamRadius  = radius;
+    beamSpeed   = speed;
+    effectTic   = 0;
 }
 
 
-void Controller::updateAuto() {
-    Color color = color2;
-    
-    if (nowTic - lastTic > (unsigned long) (stable+transition)) // check if it's time to change color
-    {
-        color1 = color2;
-        lastTic = nowTic;
-        
-        if (mode==MODE_AUTO_HSV) {
-            color2.randHSV();
-        } else {
-            color2.rand(brightness); // new color to go to
-        }
+void Controller::setFillHSV(uint16_t nTransition, uint16_t nStable, uint16_t speed, bool reverse) {
+    transition = nTransition;
+    stable     = nStable;
 
+    if (reverse) {
+        mode = MODE_FILL_HSV_REVERSE;
+    } else {
+        mode = MODE_FILL_HSV;
     }
-    else if (nowTic - lastTic < (unsigned long) transition)// check if we are still in a transition (change color accordingly)
-    {
-        //color[i] = ((long)(toColor[i]-initialColor[i])*(long)(nowTic - lastTic))/transition + initialColor[i];
-        // change color according to the linear function
-        color.fade(color1,color2, nowTic, lastTic, lastTic+transition);
+
+    beamSpeed = speed;
+    effectTic = 0;
+
+    for (int16_t i=0 ; i<size ; i++) {
+        color1t[i].randHSV();
+        colort[i].set(0,0,0); // set black background
     }
-    
-    // apply color to led strip
-    setColor(color);
 }
 
-void Controller::updateAutoSingle() {
-    int i;
 
-    for (i=0 ; i<size ; i++) {
-      colort[i] = color2t[i];
+void Controller::setEmpty(Color color, uint16_t speed, bool reverse) {
+    if (reverse) {
+        mode = MODE_EMPTY_REVERSE;
+    } else {
+        mode = MODE_EMPTY;
     }
-    
-    if (nowTic - lastTic > (unsigned long) (stable+transition)) // check if it's time to change color
-    {
-        lastTic = nowTic;
-        for (i=0 ; i<size ; i++) {
-          color1t[i] = color2t[i];
-          color2t[i].randHSV();
-        }
-    }
-    else if (nowTic - lastTic < (unsigned long) transition)// check if we are still in a transition (change color accordingly)
-    {
-        for (i=0 ; i<size ;i++) {
-          colort[i].fade(color1t[i],color2t[i], nowTic, lastTic, lastTic+transition);
-        }
-    }
-    
-    // apply color to led strip
+
+    beamSpeed = speed;
+    effectTic = 0;
+
+    color1 = color;
 }
+
+
+// ------------------------- Singles mode -------------------------------------
+
+void Controller::setAutoHSV_single(uint16_t nTransition, uint16_t nStable) {
+    mode = MODE_AUTO_HSV_SINGLE;
+
+    transition = nTransition;
+    stable     = nStable;
+}
+
+
+
+// ============================================================================
+// Set colors
+// ============================================================================
+
+void Controller::setColor(Color color) {
+    for (int16_t i=0 ; i<size ; i++) {
+        colort[i] = color;
+    }
+}
+
+
+void Controller::setColorSingle(Color color[]) {
+    for (int16_t i=0 ; i<size ; i++) {
+        colort[i] = color[i];
+    }
+}
+
+
+
+// ============================================================================
+// Update mode
+// ============================================================================
+
+// ------------------------- Full mode ----------------------------------------
 
 void Controller::updateBlink() {
-    uint32_t spent = (nowTic - lastTic);
+    uint32_t spent = nowTic - lastTic;
+
     if (spent < delay1) {
         setColor(color1);
     } else if (spent < delay1+delay2) {
@@ -117,91 +204,133 @@ void Controller::updateBlink() {
     }
 }
 
-void Controller::updateBeam() {
-    if (effectTic==0) {
-      effectTic = nowTic;
+
+void Controller::updateBreath() {
+    Color color = color1;
+    uint32_t spent = nowTic - lastTic;
+
+    if (spent < delay1) {
+        color.fade(color1, color2, spent, 0, delay1);
+    } else if (spent < delay1+delay2) {
+        color.fade(color2, color1, spent, delay1, delay1+delay2);
+    } else {
+        lastTic = nowTic;
     }
 
-    int32_t centerPixel = (nowTic-effectTic)/beamSpeed;
+    setColor(color);
+}
 
-    if (effect == EFFECT_BEAM_REVERSE) {
-      centerPixel = size - centerPixel - 1;
-      //Serial.println("r");
+
+void Controller::updateAuto() {
+    Color color = color2;
+
+    if (nowTic-lastTic > stable+transition) { // check if it's time to change color
+        color1 = color2;
+        lastTic = nowTic;
+
+        if (mode == MODE_AUTO_HSV) {
+            color2.randHSV();
+        } else {
+            color2.rand(brightness); // new color to go to
+        }
+
     }
-    //Serial.println(centerPixel);
+    else if (nowTic-lastTic < transition) {
+        // check if we are still in a transition
+        // change color according to the linear function
+        color.fade(color1,color2, nowTic, lastTic, lastTic+transition);
+    }
 
-    for (int i=centerPixel-beamRadius ; i<=centerPixel+beamRadius ; i++) {
-        if (i>=0 && i<size) {
-            colort[i] = effectColor;
-            //colort[i].mix(colort[i], beamColor,
-                          //((double) (beamRadius-abs(i-centerPixel)))/beamRadius);
+    setColor(color); // apply color to led strip
+}
+
+// ------------------------- Single mode --------------------------------------
+
+void Controller::updateAutoSingle() {
+    for (int16_t i=0 ; i<size ; i++) {
+        colort[i] = color2t[i];
+    }
+
+    if (nowTic-lastTic > stable+transition) { // check if it's time to change color
+        lastTic = nowTic;
+        for (int16_t i=0 ; i<size ; i++) {
+            color1t[i] = color2t[i];
+            color2t[i].randHSV();
+        }
+    } else if (nowTic-lastTic < transition) { // check if we are still in a transition (change color accordingly)
+        for (int16_t i=0 ; i<size ; i++) {
+            colort[i].fade(color1t[i], color2t[i], nowTic, lastTic, lastTic+transition);
         }
     }
-    
+}
+
+
+// ------------------------- Effects ------------------------------------------
+
+void Controller::updateFlash() {
+    if (nowTic-effectTic < effectDuration) {
+        setColor(effectColor);
+    } else {
+        effect = EFFECT_NONE;
+    }
+}
+
+
+void Controller::updateBeam() {
+    int16_t centerPixel = (nowTic-effectTic)/beamSpeed;
+
+    if (effect == EFFECT_BEAM_REVERSE) {
+        centerPixel = size - centerPixel - 1;
+    }
+
+    for (int16_t i=centerPixel-beamRadius ; i<=centerPixel+beamRadius ; i++) {
+        if (i>=0 && i<size) {
+            colort[i] = effectColor;
+        }
+    }
+
     if (centerPixel-beamRadius >= size || centerPixel+beamRadius<0) {
         effect = EFFECT_NONE;
     }
-    
-    
-
-
 }
 
+
 void Controller::updateFillHSV() {
-    if (effectTic==0) {
-      effectTic = nowTic;
-    }
+    int16_t centerPixel = (nowTic-effectTic)/beamSpeed;
+    int16_t dir = -1;
 
-    int32_t centerPixel = (nowTic-effectTic)/beamSpeed;
-    int dir = -1;
-    
     if (mode == MODE_FILL_HSV_REVERSE) {
-      dir = +1;
-      centerPixel = size - centerPixel - 1;
+        dir = +1;
+        centerPixel = size - centerPixel - 1;
     }
 
-    for (int i=centerPixel; i>=0 && i<size; i+= dir) {
-      colort[i] = color1t[i];
+    for (int16_t i=centerPixel; i>=0 && i<size; i+= dir) {
+        colort[i] = color1t[i];
     }
-    
+
     if (centerPixel>=size || centerPixel<0) {
         mode = MODE_AUTO_HSV_SINGLE;
         lastTic = nowTic;
     }
 }
 
+
 void Controller::updateEmpty() {
-    if (effectTic==0) {
-      effectTic = nowTic;
-    }
+    int16_t centerPixel = (nowTic-effectTic)/beamSpeed;
+    int16_t dir = +1;
 
-    int32_t centerPixel = (nowTic-effectTic)/beamSpeed;
-    int dir = +1;
-    
     if (mode == MODE_EMPTY_REVERSE) {
-      dir = -1;
+        dir = -1;
     } else {
-      centerPixel = size - centerPixel - 1;
+        centerPixel = size - centerPixel - 1;
     }
 
-    for (int i=centerPixel; i>=0 && i<size ; i+= dir) {
-      colort[i] = color1;
+    for (int16_t i=centerPixel; i>=0 && i<size ; i+= dir) {
+        colort[i] = color1;
     }
-    
+
     if (centerPixel>=size || centerPixel<0) {
         mode = MODE_MANUAL;
         lastTic = nowTic;
     }
 }
-
-void Controller::updateFlash() {
-  if (nowTic-effectTic < effectDuration) {
-    setColor(effectColor);
-  } else {
-    effect = EFFECT_NONE;
-  }
-  
-}
-
-
-
--- led_lib/Controller.h
+++ led_lib/Controller.h
@@ -6,202 +6,114 @@
 #include "Color.h"
 
 #define NUM_PIXEL 65
+#define CONTROLLER_MAX_BRIGHTNESS 255
+
 enum Mode {MODE_AUTO, MODE_BLINK, MODE_BREATH, MODE_MANUAL,
-           MODE_AUTO_HSV, MODE_AUTO_HSV_SINGLE, MODE_FILL_HSV, MODE_FILL_HSV_REVERSE,
+           MODE_AUTO_HSV, MODE_AUTO_HSV_SINGLE,
+           MODE_FILL_HSV, MODE_FILL_HSV_REVERSE,
            MODE_EMPTY, MODE_EMPTY_REVERSE};
 enum Effect {EFFECT_NONE, EFFECT_BEAM, EFFECT_BEAM_REVERSE, EFFECT_FLASH};
 
 class Controller {
   public:
-    Controller(Color nStrip[], int nSize, Color nMainColor = Color()) {
-      colort = nStrip;
-      size = nSize;
-      mainColor = nMainColor;
-    };
-
-
-    enum Mode mode;
-    enum Effect effect = EFFECT_NONE;
-    
-    int size;
-
-    uint32_t lastTic = 0;
-    uint32_t nowTic = 0;
-    uint32_t effectTic = 0;
-
-    Color effectColor;
-    int32_t beamRadius; // linearly from 100 to 0 %
-    int32_t beamSpeed; // ms per LED
-    int32_t effectDuration;
-    
-    uint32_t delay1 = 0;
-    uint32_t delay2 = 0;
-
+    // Public variable
     Color mainColor;
 
-    Color color1;
-    Color color2;
 
-    Color color1t[NUM_PIXEL];
-    Color color2t[NUM_PIXEL];
-    Color *colort;
+    // Public function
 
-    uint32_t brightness = 255; // brightness of the color (set a top boundary (max 255))
-    uint32_t transition = 900; // time to do a transition (in millisecond)
-    uint32_t stable = 100; // set how much time a color should stay before changing (in millesecond)
+    Controller(Color nStrip[], int16_t nSize, Color nMainColor = Color());
+
+    /* Set the strip to a constant manual color
+    */
+    void setManual(Color color);
+
+    /* Blink between color 1 and color 2 in succession
+     * (same as breath, but no transition anc actually stays at the color)
+     * dealy1: time it stays at color 1
+     * delay2: time it stays at color 2
+     */
+    void setBlink(Color nColor1, Color nColor2, uint32_t nDelay1, uint32_t nDelay2);
 
     /* Goes from color 1 to color 2 in succession
      * upTime : time to fade from 1 to 2
      * downTime : time to fade from 2 to 1
      * never actually stays at color 1 or 2
      */
-
-    void setBreath(Color nColor1, Color nColor2, unsigned int upTime, unsigned int downTime) {
-        mode = MODE_BREATH;
-        color1 = nColor1;
-        color2 = nColor2;
-
-        delay1 = upTime;
-        delay2 = downTime;
-    };
+    void setBreath(Color nColor1, Color nColor2, uint16_t upTime, uint16_t downTime);
 
     /* Transition from one random color to the next
      * transition: time it takes to go from one color to the next
      * stable: time it stays at one color
      * brightness: max brightness for the random color
      */
-    void setAuto(unsigned int nTransition, unsigned int nStable, unsigned int nBrightness) {
-        mode = MODE_AUTO;
-
-        if (nBrightness > 255) {
-            nBrightness = 255;
-        }
-
-        brightness = nBrightness;
-        transition = nTransition;
-        stable     = nStable;
-    };
+    void setAuto(uint16_t nTransition, uint16_t nStable, uint8_t nBrightness);
 
     /* Transition from one random color to the next
      * transition: time it takes to go from one color to the next
      * stable: time it stays at one color
      * brightness: max brightness for the random color
      */
-    void setAutoHSV(unsigned int nTransition, unsigned int nStable) {
-        mode = MODE_AUTO_HSV;
+    void setAutoHSV(uint16_t nTransition, uint16_t nStable);
 
-        transition = nTransition;
-        stable     = nStable;
-    };
+    void setAutoHSV_single(uint16_t nTransition, uint16_t nStable);
 
-    void setAutoHSV_single(unsigned int nTransition, unsigned int nStable) {
-        mode = MODE_AUTO_HSV_SINGLE;
 
-        transition = nTransition;
-        stable     = nStable;
-    };
+    void setEffectFlash(Color color, uint32_t duration);
+    void setEffectBeam(Color color, uint16_t radius, uint16_t speed, bool reverse=false);
+    void setFillHSV(uint16_t nTransition, uint16_t nStable,
+            uint16_t speed, bool reverse=false);
+    void setEmpty(Color color, uint16_t speed, bool reverse=false);
 
-    /* Set the strip to a constant manual color
-     */
-    void setManual(Color color) {
-        mode = MODE_MANUAL;
-        color1 = color;
-    }
 
-    void setEffectBeam(Color color, uint32_t radius, uint32_t speed, bool reverse=false) {
-        if (reverse) {
-          effect = EFFECT_BEAM_REVERSE;
-        } else {
-          effect = EFFECT_BEAM;
-        }
-        effectColor = color;
-        beamRadius = radius;
-        beamSpeed = speed;
-        effectTic = 0;
-    }
-
-    void setFillHSV(unsigned int nTransition, unsigned int nStable,
-                    uint32_t speed, bool reverse=false) {
-        transition = nTransition;
-        stable     = nStable;
-        
-        if (reverse) {
-          mode = MODE_FILL_HSV_REVERSE;
-        } else {
-          mode = MODE_FILL_HSV;
-        }
-        
-        beamSpeed = speed;
-        effectTic = 0;
-
-        for (int i=0 ; i<size ; i++) {
-          color1t[i].randHSV();
-          colort[i].set(0,0,0); // set black background
-        }
-    }
-
-    void setEmpty(Color color, uint32_t speed, bool reverse=false) {
-        if (reverse) {
-          mode = MODE_EMPTY_REVERSE;
-        } else {
-          mode = MODE_EMPTY;
-        }
-        
-        beamSpeed = speed;
-        effectTic = 0;
-
-        color1 = color;
-    }
-
-    void setEffectFlash(Color color, int32_t duration) {
-        effect = EFFECT_FLASH;
-        
-        effectColor = color;
-        effectDuration = duration;
-        
-        effectTic = nowTic;
-    }
-    
-    /* Blink between color 1 and color 2 in succession
-     * (same as breath, but no transition anc actually stays at the color)
-     * dealy1: time it stays at color 1
-     * delay2: time it stays at color 2
-     */
-    void setBlink(Color nColor1, Color nColor2, uint32_t nDelay1, uint32_t nDelay2) {
-        mode = MODE_BLINK;
-
-        color1 = nColor1;
-        color2 = nColor2;
-
-        delay1 = nDelay1;
-        delay2 = nDelay2;
-    }
-
-    void setColor(Color color) {
-        for (int i=0 ; i<size ; i++) {
-            colort[i] = color;
-        }
-    };
-
-    void setColorSingle(Color color[]) {
-        for (int i=0 ; i<size ; i++) {
-            colort[i] = color[i];
-        }
-    };
+    void setColor(Color color);
+    void setColorSingle(Color color[]);
 
     void update(uint32_t now);
 
-    private:
-      void updateBreath();
-      void updateAuto();
-      void updateBlink();
-      void updateAutoSingle();
-      void updateBeam();
-      void updateFlash();
-      void updateFillHSV();
-      void updateEmpty();
+  private:
+    enum Mode mode = MODE_AUTO;
+    enum Effect effect = EFFECT_NONE;
+    
+    int16_t size;
+
+    uint32_t lastTic = 0;
+    uint32_t nowTic = 0;
+    uint32_t effectTic = 0;
+
+    int16_t beamRadius; // linearly from 100 to 0 %
+    int16_t beamSpeed; // ms per LED
+    uint32_t effectDuration;
+
+    uint32_t delay1 = 0;
+    uint32_t delay2 = 0;
+
+
+    Color color1;
+    Color color2;
+    Color effectColor;
+
+    Color color1t[NUM_PIXEL];
+    Color color2t[NUM_PIXEL];
+    Color *colort;
+
+    uint8_t brightness = CONTROLLER_MAX_BRIGHTNESS; // brightness of the color (set a top boundary (max 255))
+    uint16_t transition = 900; // time to do a transition (in millisecond)
+    uint16_t stable = 100; // set how much time a color should stay before changing (in millesecond)
+
+    // Private function
+
+    void updateBlink();
+    void updateBreath();
+    void updateAuto();
+
+    void updateAutoSingle();
+
+    void updateFlash();
+    void updateBeam();
+    void updateFillHSV();
+    void updateEmpty();
 
 };
-
 
 #endif
